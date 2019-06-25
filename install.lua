66fb478621677e9291c0b9421d9e31a397312437
RobertNiklasBock <robert-niklas.bock@tu-ilmenau.de>
2019-06-25 11:47:47 +0200

    DUM?

--- install.lua
+++ install.lua
@@ -1,4 +1,4 @@
-﻿shell.run("rm save.lua")
+shell.run("rm save.lua")
 shell.run("rm utils.lua")
 
 shell.run("wget https://raw.githubusercontent.com/RobertNiklasBock/cc-scripts/master/save.lua save.lua")
--- save.lua
+++ save.lua
@@ -49,7 +49,7 @@ function save.forward(times)
     end
 end
 
-function save.up()(times)
+function save.up(times)
     times = utils.optional(times, 1)
 
     for i=1,times do
