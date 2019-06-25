685dad8320f079bc7600a410b9ae9c7680b48787
RobertNiklasBock <robert-niklas.bock@tu-ilmenau.de>
2019-06-25 11:37:32 +0200

    added installer

---
+++ install.lua
@@ -0,0 +1,5 @@
+﻿rm save.lua
+rm utils.lua
+
+wget https://raw.githubusercontent.com/RobertNiklasBock/cc-scripts/master/save.lua save.lua
+wget https://raw.githubusercontent.com/RobertNiklasBock/cc-scripts/master/utils.lua utils.lua
