315fef16d39d481b8b73fee339d2191672fcaf6c
RobertNiklasBock <robert-niklas.bock@tu-ilmenau.de>
2019-06-25 11:43:33 +0200

    using shell.run now

--- install.lua
+++ install.lua
@@ -1,5 +1,5 @@
-﻿os.remove("save.lua")
-os.remove("utils.lua")
+﻿shell.run("rm save.lua")
+shell.run("rm utils.lua")
 
-wget https://raw.githubusercontent.com/RobertNiklasBock/cc-scripts/master/save.lua save.lua
-wget https://raw.githubusercontent.com/RobertNiklasBock/cc-scripts/master/utils.lua utils.lua
+shell.run("wget https://raw.githubusercontent.com/RobertNiklasBock/cc-scripts/master/save.lua save.lua")
+shell.run("wget https://raw.githubusercontent.com/RobertNiklasBock/cc-scripts/master/utils.lua utils.lua")
