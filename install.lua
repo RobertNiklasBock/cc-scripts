81b7f26852750eb5a68d1d4f67d6e072f3e4a8cc
RobertNiklasBock <robert-niklas.bock@tu-ilmenau.de>
2019-06-25 11:59:46 +0200

    added strip mining, patched install?

--- install.lua
+++ install.lua
@@ -1,5 +1,7 @@
 shell.run("rm save.lua")
 shell.run("rm utils.lua")
+shell.run("rm install.lua")
 
 shell.run("wget https://raw.githubusercontent.com/RobertNiklasBock/cc-scripts/master/save.lua save.lua")
 shell.run("wget https://raw.githubusercontent.com/RobertNiklasBock/cc-scripts/master/utils.lua utils.lua")
+shell.run("wget https://raw.githubusercontent.com/RobertNiklasBock/cc-scripts/master/install.lua install.lua")
---
+++ strip.lua
@@ -0,0 +1,20 @@
+﻿local save = require "save"
+
+local cobbleSlot = 16
+
+function segment()
+    turtle.select(cobbleSlot)
+    save.forward()
+    turtle.placeUp()
+    save.down()
+    turtle.placeDown()
+    save.forward()
+    turtle.placeDown()
+    save.up()
+    turtle.placeUp()
+    save.forward()
+    turtle.placeUp()
+    save.down()
+    turtle.placeDown()
+
+segment()
