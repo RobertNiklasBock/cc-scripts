45b0f0eb243fdc95a22b98354f9b4cd80a0e77e6
RobertNiklasBock <robert-niklas.bock@tu-ilmenau.de>
2019-06-25 15:40:42 +0200

    updatet installer, imporved save operatione - trying to aoid sleep

--- install.lua
+++ install.lua
@@ -1,9 +1,30 @@
-shell.run("rm save.lua")
-shell.run("rm utils.lua")
-shell.run("rm install.lua")
-shell.run("rm strip.lua")
+local base = "https://raw.githubusercontent.com/RobertNiklasBock/cc-scripts/master/"
 
-shell.run("wget https://raw.githubusercontent.com/RobertNiklasBock/cc-scripts/master/save.lua save.lua")
+function wget(file)
+    shell.run("wget " .. base .. file .. " " .. file)
+end
+
+
+function main()
+    shell.run("rm install.txt")
+    wget("install.txt")
+
+    for line in io.lines("install.txt") do
+        shell.run("rm " .. line)
+        print "Getting " .. line
+        wget(line)
+
+    end
+end
+
+
+
+
+
+
+
+
+
 shell.run("wget https://raw.githubusercontent.com/RobertNiklasBock/cc-scripts/master/utils.lua utils.lua")
 shell.run("wget https://raw.githubusercontent.com/RobertNiklasBock/cc-scripts/master/install.lua install.lua")
 shell.run("wget https://raw.githubusercontent.com/RobertNiklasBock/cc-scripts/master/strip.lua strip.lua")
