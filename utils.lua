5e5216e428e7dff717f498e8583d61dad794703d
RobertNiklasBock <robert-niklas.bock@tu-ilmenau.de>
2019-06-26 22:13:04 +0200

    Added utils for tables and Inventory sorting

--- utils.lua
+++ utils.lua
@@ -1,4 +1,4 @@
-local utils = {}
+﻿local utils = {}
 
 function utils.compressInventory(blacklist)
     local previousSlot = turtle.getSelectedSlot()
@@ -15,20 +15,70 @@ function utils.compressInventory(blacklist)
             turtle.select(i)
 
             for j=1,i-1 do
-                if turtle.getItemCout() == 0 then
+                if turtle.getItemCount() == 0 then
                     break
                 end
                 turtle.transferTo(j)
             end
         end
     end
+    turtle.select(previousSlot)
 end
 
-function utils.isInventoryFull()
+function utils.organizeInventory(matchingTable)
+    local previousSlot = turtle.getSelectedSlot()
+    local freeSlot = utils.getFreeSlot()
+
+    if freeSlot == 0 then
+        return false
+    end
+
+    for k,v in ipairs(matchingTable) do
+        if not utils.getItemName(k) == v then
+            turtle.transferTo(freeSlot)
+        end
+
+        for i=1,16 do
+            -- check whether this slot is in sorting table -> we dont want to break existing order
+            if matchingTable[i] == nil then
+                turtle.select(i)
+                turtle.transferTo(k)
+                -- if items were left behind, the target slot is full -> break early
+                if turtle.getItemCount() != 0 then
+                    break
+                -- otherwise we have a new, definitively free slot
+                else
+                    freeSlot = i
+                end
+            end
+        end
+    end
+
+function utils.getFreeSlot()
     local previousSlot = turtle.getSelectedSlot()
     for i=1,16 do
         turtle.select(i)
+        if turtle.getItemCount() == 0 then
+            return i
+        end
     end
+
+    return 0
+end
+
+function utils.getItemName(slot)
+    local detail = turtle.getItemDetail(slot)
+    return detail.name or ""
+end
+
+function utils.isInList(item, list)
+    for k,v in ipairs(list) do
+        if item == v then
+            return k
+        end
+    end
+
+    return nil
 end
 
 return utils
